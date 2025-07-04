5fd39a01189a312587eb97fa7498515011375264
ahxin0506 <tiongingxin0506@gmail.com>
2025-07-04 16:01:53 +0800

    Update pugongyingdetails.php

--- pugongyingdetails.php
+++ pugongyingdetails.php
@@ -11,20 +11,20 @@
 
 <body>
    <br />
-<b>Notice</b>:  Undefined index: name in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>17</b><br />
+<b>Notice</b>:  Undefined index: name in <b>pugongyingdetails.php</b> on line <b>17</b><br />
 <br />
-<b>Notice</b>:  Undefined index: email in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>18</b><br />
+<b>Notice</b>:  Undefined index: email in <b>pugongyingdetails.php</b> on line <b>18</b><br />
 <br />
-<b>Notice</b>:  Undefined index: phone in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>19</b><br />
+<b>Notice</b>:  Undefined index: phone in <b>pugongyingdetails.php</b> on line <b>19</b><br />
 <br />
-<b>Notice</b>:  Undefined index: date in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>20</b><br />
+<b>Notice</b>:  Undefined index: date in <b>pugongyingdetails.php</b> on line <b>20</b><br />
 <br />
-<b>Notice</b>:  Undefined index: date1 in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>21</b><br />
+<b>Notice</b>:  Undefined index: date1 in <b>pugongyingdetails.php</b> on line <b>21</b><br />
 <br />
-<b>Notice</b>:  Undefined index: type in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>22</b><br />
+<b>Notice</b>:  Undefined index: type in <b>pugongyingdetails.php</b> on line <b>22</b><br />
 <br />
-<b>Notice</b>:  Undefined index: number in <b>/home/a0006/public_html/Mid-Term/pugongyingdetails.php</b> on line <b>23</b><br />
-<center><a href="http://120.107.152.110/~a0006/Mid-Term/pugongyingdetails.html">抱歉請重試</a><br></body>
+<b>Notice</b>:  Undefined index: number in <b>pugongyingdetails.php</b> on line <b>23</b><br />
+<center><a href="pugongyingdetails.html">抱歉請重試</a><br></body>
 
 
 </html>
